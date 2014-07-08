0f03392512e076d0615bea79b29f07c683f8f0a2
Okan SAYILGAN <osayilgan@gmail.com>
2014-07-09 01:47:08 +0300

    Updated : New Client Key and Secret Added : API Url as Constant

--- BreezyAndroid/src/com/breezy/oauth/Constants.java
+++ BreezyAndroid/src/com/breezy/oauth/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
 	
 	public static final String USER_EMAIL = "test@breezy-test.com";
 	public static final String PASSWORD = "Password11";
-	public static final String CLIENT_KEY = "OnFgci4TKdfwsPeLDpSdeFMwYFJLu0dYyCkDOKSk";
-	public static final String CLIENT_SECRET = "RokABb5bcc158x1pLKAQjlIKNAvcUjU3ZgHXaRW6";
+	public static final String CLIENT_KEY = "DP-GqheU93sSMKfG2Ao0rFhaOHcxAmSQLPoMclbhjikip1RNXsZFN1pLxNYwQmPDtMz";
+	public static final String CLIENT_SECRET = "xr49R3XH2BkzBrv0vVTcFPazjymhTbWsfx22zaqGe9Li7yyZM8bJMPAA4OUlzMLX";
+	
+	public static final String API_URL = "http://breezy-api-test.azurewebsites.net/";
 }
