aa3a072133322233a8467fe23032fc3a9c52567b
Okan SAYILGAN <osayilgan@gmail.com>
2014-07-10 05:13:53 +0300

    Added : Step 2 Logic from OAuth, to get the oauth_verifier response

--- BreezyAndroid/src/com/breezy/oauth/Constants.java
+++ BreezyAndroid/src/com/breezy/oauth/Constants.java
@@ -11,6 +11,4 @@ public class Constants {
 	public static final String PASSWORD = "Password11";
 	public static final String CLIENT_KEY = "DP-GqheU93sSMKfG2Ao0rFhaOHcxAmSQLPoMclbhjikip1RNXsZFN1pLxNYwQmPDtMz";
 	public static final String CLIENT_SECRET = "xr49R3XH2BkzBrv0vVTcFPazjymhTbWsfx22zaqGe9Li7yyZM8bJMPAA4OUlzMLX";
-	
-	public static final String API_URL = "http://breezy-api-test.azurewebsites.net/";
 }
--- BreezyAndroid/src/com/breezy/oauth/MainActivity.java
+++ BreezyAndroid/src/com/breezy/oauth/MainActivity.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -20,6 +22,10 @@ import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthConstants;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.RequestTuner;
+import org.scribe.model.Response;
 import org.scribe.model.Token;
 import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
@@ -40,13 +46,13 @@ public class MainActivity extends Activity implements OnClickListener {
 	private EditText userName, password;
 	private Button loginButton;
 	
+	/** API Class holds URL and API Parameters */
+	final BreezyApi api = new BreezyApi();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy); 
 		
 		/* initialize UI Elements */
 		initUI();
@@ -100,114 +106,146 @@ public class MainActivity extends Activity implements OnClickListener {
 				
 				/* Trying to get the Request Token First */
 				OAuthService service = new ServiceBuilder()
-				.provider(BreezyApi.class)
+				.provider(api)
 				.apiKey(Constants.CLIENT_KEY)
 				.apiSecret(Constants.CLIENT_SECRET)
 				.callback("oob")
 				.build();
 				
-				Scanner in = new Scanner(System.in);
-				
+				/* Step 1 */
+				/* Get Request Token */
 				Token requestToken = service.getRequestToken();
-				Log.i("Okan", "Request Token : " + requestToken.toString());
+				Log.i("Okan", "(1)Request Token : " + requestToken.getToken());
+				Log.i("Okan", "(2)Request Token Secret : " + requestToken.getSecret());
 				
 				String authUrl = service.getAuthorizationUrl(requestToken);
 				Log.i("Okan", "OAuth URL : " + authUrl);
 				
-				/* Fails here, NoSuchElementException */
-				String verifierString = in.nextLine();
-				Log.i("Okan", "Verifier : " + verifierString);
+				/* Step 2 */
+				/* Get Verification Code, make custom call to API with request token and user credentials */
+				String verifierValue = getOAuthVerifier(requestToken.getToken());
+				Log.i("Okan", "Verifier String : " + verifierValue);
 				
-				Verifier verifier = new Verifier(verifierString);
+				/* Step 3 */
+				/* Get Access token with Request Token and Verification Code */
+				/* Save Access Token */
+				
+				/** FAILS HERE, oauth_verifier is not correct **/
+				/* Check the Response message in Log Cat */
+				Verifier verifier = new Verifier(verifierValue);
 				Token accessToken = service.getAccessToken(requestToken, verifier);
 				Log.i("Okan", "Access Token : " + accessToken.toString());
 				
 				Looper.loop();
-				
 			}
 		}).start();
 	}
 	
-	public void authenticateUser() {
+	/**
+	 * Sends POST Request to API with Request Token and User Credentials.
+	 * 
+	 * @param requestToken	Request Token retrieved at Step 1.
+	 * @return				Returns oauth_verifier String from API Response.
+	 */
+	public String getOAuthVerifier(String requestToken) {
 		
-		final String url = "http://breezy-api-test.azurewebsites.net/oauth/request_token";
+		/* URL with Request Token */
+		String url = "http://breezy-api-test.azurewebsites.net/oauth/authorize?oauth_token=" + requestToken;
+		String oauthVerifier = "";
 		
-		// Insert Loading indicator inside the button.
+		/* Create JSON Object with User Credentials */
+		JSONObject postMessage = new JSONObject();
+		try {
+			postMessage.put("password", Constants.PASSWORD);
+			postMessage.put("email", Constants.USER_EMAIL);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		
-		// Authenticate inside a Thread.
+		/* HTTP Client and it's parameters */
+		int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
+		HttpParams httpParams = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
+		HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
+		HttpClient client = new DefaultHttpClient(httpParams);
 		
-		new Thread(new Runnable() {
+		HttpPost request = new HttpPost(url);
+		
+		/* Entity of Request */
+		StringEntity se;
+		try {
 			
-			@Override
-			public void run() {
+			String body = postMessage.toString();
+			se = new StringEntity(body);
+			se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            request.setEntity(se);
+            
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		HttpResponse response = null;
+		
+		/* Execute Request */
+		try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		/*Checking response */
+        if(response != null){
+            try {
+            	
+            	BufferedReader inputStream = null;
 				
-				Looper.prepare();
+				inputStream = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+				StringBuilder stringBuilder = new StringBuilder();
+				String line = null;
 				
-				JSONObject postMessage = new JSONObject();
-				try {
-					postMessage.put("password", Constants.PASSWORD);
-					postMessage.put("client_id", 1);
-					postMessage.put("cluster_id", 1);
-					postMessage.put("email", Constants.USER_EMAIL);
-				} catch (JSONException e) {
-					e.printStackTrace();
+				while ((line = inputStream.readLine()) != null) {
+					stringBuilder.append(line + "\n");
 				}
 				
-				int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
-				HttpParams httpParams = new BasicHttpParams();
-				HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
-				HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
-				HttpClient client = new DefaultHttpClient(httpParams);
+				/* LOG full Response */
+				Log.i("Okan", "Full Response : " + stringBuilder.toString());
 				
-				HttpPost request = new HttpPost(url);
+				/* Get Verifier */
+				oauthVerifier = parseOAuthVerifier(stringBuilder.toString());
 				
-				StringEntity se;
-				try {
-					
-					se = new StringEntity(postMessage.toString());
-					se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-	                request.setEntity(se);
-	                
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
+				Log.i("Okan", "Verifier : " + oauthVerifier);
 				
-				HttpResponse response = null;
-				
-				try {
-					response = client.execute(request);
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-				/*Checking response */
-                if(response!=null){
-                    try {
-                    	
-                    	BufferedReader inputStream = null;
-						
-						inputStream = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-						StringBuilder stringBuilder = new StringBuilder();
-						String line = null;
-						
-						while ((line = inputStream.readLine()) != null) {
-							stringBuilder.append(line + "\n");
-						}
-						
-						Log.i("Okan", "Response : " + stringBuilder.toString());
-						
-						
-					} catch (IllegalStateException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                }
-				
-                Looper.loop();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		}).start();
+        }
+        
+        return oauthVerifier;
+	}
+	
+	/**
+	 * Parses API Response which has oauth_token and oauth_verifier Strings with Regex.
+	 * 
+	 * @param input	API Response
+	 * @return		Returns oauth_verifier value,
+	 */
+	public String parseOAuthVerifier(String input) {
+		
+		String oauth_verifier = "";
+		
+		Pattern p = Pattern.compile("(?:&([^=]*)=([^&]*))");
+		Matcher m = p.matcher(input);
+		while (m.find()) {
+			
+			if (m.group(1).equals("oauth_verifier")) {
+				oauth_verifier = m.group(2);
+			}
+		}
+		
+		return oauth_verifier;
 	}
 }
